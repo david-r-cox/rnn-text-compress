2fc6d4209bc78a4f2f18eedbea67d70be199806f
david <dcox@paloaltonetworks.com>
2016-07-28 19:22:53 -0700

    re-added timestamp feature

--- gru_text_predict.py
+++ gru_text_predict.py
@@ -11,6 +11,7 @@ from os import devnull
 import numpy as np
 import random
 import shlex
+import time
 import sys
 
 # Cython imports:
@@ -103,9 +104,10 @@ def load_weights(model):
 
 # train the model (for debugging only. use weights file instead.)
 def train(model, X_chars, X_pos, y, num_iterations=150):
+    start_t = int(time.time())
     for i in xrange(num_iterations / 10):
         print('Iteration:', i)
-        checkpoint_name = '_'.join([basename(input_file_path), str(i*10), 'weights.hdf5'])
+        checkpoint_name = '_'.join([basename(input_file_path),start_t, str((i+1)*10), 'weights.hdf5'])
         checkpointer = ModelCheckpoint(filepath=checkpoint_name)
         model.fit([X_chars, X_pos], y,
                   batch_size=512,
